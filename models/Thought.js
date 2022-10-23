bb90ce00ae5ee16e9c8c7ce694449d908e9fca6e
LujanSolo <lujansolo@proton.me>
2022-10-22 20:20:58 -0700

    moved reaction code into thought.js model as a subdocument. deleted Reaction.js model file

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,29 @@
 const { Schema, model } = require ('mongoose');
 
+
+//* 'reaction' subdocument to be embedded into the parent -> Thought document
+const reactionSchema = new Schema (
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      maxLength: 280,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }
+)
+
 const thoughtSchema = new Schema (
   {
     thoughtText: {
