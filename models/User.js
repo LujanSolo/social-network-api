0bb2b96e8a85885e15a5183411066c01c7bebcc4
LujanSolo <lujansolo@proton.me>
2022-10-22 19:28:01 -0700

    added user.js in models. todo: add a virtual

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require ('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+//todo Create a virtual called "friendCount" that retrieves length of user's friends array on query
+
+const User = model('user', userSchema);
+
+module.exports = User;
